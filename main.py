2eab6cce59de6f9390e1b4402b8068cca71a4020
YashashwiniSiwach26 <yashashwini2612@gmail.com>
2026-07-14 15:51:24 +0530

    Day 2: Python fundamentals

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 print("=" * 50)
 print("🚀 Welcome to DataLens AI")
-print("Building ML from Scratch")
+print("Building ML" )
 print("=" * 50)
+name="Yashashwini"
+age=20
+cgpa=8.81
+print("\nUser Information:")
+print(f"name: {name}, age: {age}, cgpa: {cgpa}")
+skills=["Python","Machine Learning","Git","Data Analysis"]
+languages=["Python","C++","C","Java"]
+print("\nskills:")
+for skill in skills:
+    print(f"skill:{skill}")
+print("\nlanguages:")
+for language in languages:
+    print(f"language:{language}")
+student={"name":name,"age":age,"cgpa":cgpa}
+print("\nStudent Information:")
+print(f"name:{student['name']},age:{student['age']},cgpa:{student['cgpa']}")
+def greet():
+    print("Welcome to my project!")
+greet()
+
+
