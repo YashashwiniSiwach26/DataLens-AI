f7ba839362f0e38bc0169b34ea13d7b1cf20e4e7
YashashwiniSiwach26 <yashashwini2612@gmail.com>
2026-07-25 14:09:32 +0530

    Day14: Random Forest Classifier and Feature Importance

---
+++ notebooks/day14_random_forest.py
@@ -0,0 +1,37 @@
+#ensemble Learning: a group working together
+#Random Forest is an ensemble learning algo because it combines multiple Decision trees
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import(
+    accuracy_score,
+    confusion_matrix,
+    classification_report
+)
+data={
+    "Hours":[1,2,3,4,5,6,7,8,9,10,11,12],
+    "Pass":[0,0,0,0,1,1,1,1,1,1,1,1]
+}
+df=pd.DataFrame(data)
+print(df)
+
+X=df[["Hours"]]
+y=df["Pass"]
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+model=RandomForestClassifier(n_estimators=100,max_depth=3,random_state=42)
+model.fit(X_train,y_train)
+predictions=model.predict(X_test)
+print(predictions)
+accuracy=accuracy_score(y_test,predictions)
+print("Accuracy",accuracy)
+cm=confusion_matrix(y_test,predictions)
+print("\nConfusion Matrix")
+print(cm)
+print("\nClassification Report")
+print(classification_report(y_test,predictions))
+prediction=model.predict([[6.5]])
+print("Prediction:",prediction)
+importance=model.feature_importances_
+print("Feature Importance:",importance)
