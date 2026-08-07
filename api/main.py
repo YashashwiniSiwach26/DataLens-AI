163b274133500609140381766cc875fe26ca46fc
YashashwiniSiwach26 <yashashwini2612@gmail.com>
2026-08-07 16:24:45 +0530

    Day 27 auto ml model training

--- api/main.py
+++ api/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI, UploadFile, File
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import joblib
 
 # Create FastAPI application
 app = FastAPI()
@@ -76,8 +85,26 @@ def upload_dataset(file: UploadFile = File(...)):
     if numerical_columns:
         df[numerical_columns] = scaler.fit_transform(df[numerical_columns])
 
-    
+    X=df[feature_columns]
+    y=df[target_column]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    models={
+        "Logistic Regression": LogisticRegression(),
+        "Decision Tree": DecisionTreeClassifier(),
+        "Random Forest": RandomForestClassifier(),
+        "SVM": SVC(),
+        "KNN": KNeighborsClassifier(n_neighbors=3),
+        "Naive Bayes": GaussianNB()}
+    scores={}
+    for name,model in models.items():
+        model.fit(X_train,y_train)
+        predictions=model.predict(X_test)
+        accuracy=accuracy_score(y_test,predictions)
+        scores[name]=round(accuracy*100, 2)
 
+    best_model_name=max(scores,key=scores.get)
+    best_model=models[best_model_name]
+    joblib.dump(best_model,"best_model.pkl")    
     return {
         "filename": file.filename,
         "rows": rows,
@@ -92,7 +119,15 @@ def upload_dataset(file: UploadFile = File(...)):
         "target_column": target_column,
         "preview": df.head().to_dict(orient="records"),
         "encoded_preview": df.head().to_dict(orient="records"),
-        "label_mapping": label_encoders
+        "label_mapping": label_encoders,
+        "model_scores": scores,
+        "best_model": best_model_name
     }
 
-
+@app.post("/predict")
+def predict(data:dict):
+    model=joblib.load("best_model.pkl")
+    input_data=pd.DataFrame([data])
+    prediction=model.predict(input_data)
+    return {"prediction": int(prediction[0])
+        }
